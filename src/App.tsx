3abc562d930f617db275757d0e1feed9f0c22ec2
Wasima230Sultana <wasimasultana230@gmail.com>
2026-09-12 07:36:39 +0600

    Project setup

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+     <h2 className='font-bold text-red-950'>Hello World</h2>
+     <button className="btn btn-soft btn-success">Success</button>
+
+    </>
+  )
+}
+
+export default App
